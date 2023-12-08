29afd5a0d5fbca4f7bb3d1acedf36c289f89f22e
thomas diasalves <26359654+diasalvt@users.noreply.github.com>
2023-12-08 15:05:06 +0100

    day 3 part 2

--- 2023/3/solution.py
+++ 2023/3/solution.py
@@ -1,4 +1,6 @@
 import re
+from collections import defaultdict
+from math import prod
 
 
 def load(filename: str) -> list[str]:
@@ -32,9 +34,9 @@ def neighborhood(
     schematic: list[str]
 ) -> str:
     return (
-        schematic[line_number - 1][start - 1:end + 1] +
-        schematic[line_number][start - 1] + schematic[line_number][end] +
-        schematic[line_number + 1][start - 1:end + 1]
+        schematic[line_number - 1][start - 1:end + 2] +
+        schematic[line_number][start - 1] + schematic[line_number][end + 1] +
+        schematic[line_number + 1][start - 1:end + 2]
     )
 
 
@@ -42,6 +44,7 @@ def sum_numbers_line(i: int, schematic: list[str]) -> list[int]:
     res = []
     for match in re.finditer(r'\d+', schematic[i]):
         start, end = match.span()
+        end = end - 1
         if re.findall(r'[^\d\.]', neighborhood(start, end, i, schematic)):
             res.append(
                 int(match.group(0))
@@ -54,4 +57,54 @@ print(
         sum_numbers_line(i, padded_schematic)
         for i in range(1, len(schematic) + 1)
     )
+)
+
+
+def neighborhood_2(
+    start: int,
+    end: int,
+    line_number: int,
+    schematic: list[str]
+) -> tuple[str, str, str]:
+    return (
+        schematic[line_number - 1][start - 1:end + 2],
+        (
+            schematic[line_number][start - 1] +
+            ('.' * (end - start + 1)) +
+            schematic[line_number][end + 1]
+        ),
+        schematic[line_number + 1][start - 1:end + 2]
+    )
+
+
+Pos = tuple[int, int]
+
+
+def asterix_numbers_line(
+    i: int, schematic: list[str], observed: defaultdict[list]
+) -> defaultdict[Pos, list[int]]:
+    for match in re.finditer(r'\d+', schematic[i]):
+        start, end = match.span()
+        end = end - 1
+        for j, line in enumerate(neighborhood_2(start, end, i, schematic), -1):
+            for asterix_match in re.finditer(r'\*', line):
+                pos = (i + j, start + asterix_match.start() - 1)
+                observed[pos].append(int(match.group(0)))
+    return observed
+
+
+def compute_gears(schematic: list[str]) -> int:
+    asterix_neighbours = defaultdict(list)
+    for i in range(1, len(pad(schematic))):
+        asterix_neighbours = asterix_numbers_line(i, pad(schematic), asterix_neighbours)
+
+    return sum(prod(numbers) for asterix_pos, numbers in asterix_neighbours.items() if len(numbers) > 1)
+
+
+def test_compute_gears():
+    compute_gears(pad(schematic_test)) == 467835
+
+
+print(
+    compute_gears(padded_schematic)
 )
