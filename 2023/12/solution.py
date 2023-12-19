6410bdb24c0f1ed9e579e62ce04daa8c5940f6e8
thomas diasalves <26359654+diasalvt@users.noreply.github.com>
2023-12-19 21:50:04 +0100

    day 12

--- 2023/12/solution.py
+++ 2023/12/solution.py
@@ -1,6 +1,5 @@
-from typing import Generator, Callable
+from typing import Generator, Callable, Sequence, Any
 from functools import cache
-from tqdm import tqdm
 
 
 def parse(line: str) -> tuple[str, list[int]]:
@@ -64,6 +63,7 @@ def dots_sharps(pattern: str, conditions: tuple[int], min_val: int = 0) -> list:
                 ]
         return res
 
+
 print(
     sum(
         len(list(dots_sharps_gen(pattern, tuple(conditions))))
@@ -80,15 +80,68 @@ print(
 )
 
 
-def augment(f: Callable) -> Callable:
+def augment(f: Callable, repeat: int = 1) -> Callable:
     def augmented_f(pattern, conditions, *args, **kwargs):
-        return f('?'.join([pattern]*5), tuple(conditions)*5, *args, *kwargs)
+        return f(
+            '?'.join([pattern]*repeat),
+            tuple(conditions)*repeat, *args, *kwargs
+        )
     return augmented_f
+
+
+def split_pivot(seq: Sequence, i: int, l: int = 1) -> tuple[Sequence, Any, Sequence]:
+    return seq[:i], seq[i: i + l], seq[i + l:]
+
+def itersplit(seq: Sequence, l: int = 1) -> Generator:
+    for i in range(len(seq)):
+        yield split_pivot(seq, i, l)
+
+
+@cache
+def count_dots_sharps(pattern: str, conditions: tuple) -> int:
+    if not pattern:
+        return 1 if not conditions else 0
+    if not conditions:
+        return 1 if '#' not in pattern else 0
+    if len(pattern) < (sum(conditions) + len(conditions) - 1):
+        return 0
+
+    pivot_idx = len(pattern) // 2
+    pattern_before, pivot, pattern_after = split_pivot(pattern, pivot_idx)
+    match pivot:
+        case '.':
+            return sum(
+                (
+                    count_dots_sharps(pattern_before, conditions[:i]) *
+                    count_dots_sharps(pattern_after, conditions[i:])
+                )
+                for i in range(len(conditions) + 1)
+            )
+        case '#':
+            res = 0
+            pattern = '.' + pattern_before + 'P' + pattern_after + '.' # Padding pattern
+            for cond_before, (cond,), cond_after in itersplit(conditions):
+                for p_before, to_match, p_after in itersplit(pattern, cond + 2):
+                    if 'P' in to_match:
+                        to_match = to_match.replace('P', '#')
+                        if is_valid(to_match, '.' + '#' * cond + '.'):
+                            res += (
+                                count_dots_sharps(p_before, cond_before) *
+                                count_dots_sharps(p_after, cond_after)
+                            )
+            return res
+        case '?':
+            pattern_dot = pattern_before + '.' + pattern_after
+            pattern_sharp = pattern_before + '#' + pattern_after
+            return (
+                count_dots_sharps(pattern_dot, conditions) +
+                count_dots_sharps(pattern_sharp, conditions)
+            )
 
 
 print(
     sum(
-        sum(1 for x in augment(dots_sharps)(pattern, conditions))
-        for pattern, conditions in tqdm(game_test)
+        augment(count_dots_sharps, 5)(pattern, tuple(conditions))
+        for pattern, conditions in game
     )
 )
