390d04ab2dbdbb66232e81fc11d0fc2b72202900
thomas diasalves <26359654+diasalvt@users.noreply.github.com>
2024-01-10 18:39:52 +0100

    day 16 start

---
+++ 2023/16/solution.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from typing import Any, Self
+
+
+Pos = tuple[int, int]
+
+
+@dataclass
+class Array:
+    arr: list[list[Any]]
+
+    height = property(lambda self: len(self.arr))
+    width = property(lambda self: len(self.arr[0]))
+
+    @classmethod
+    def from_str(cls, s: str) -> Self:
+        return cls([list(line) for line in s.splitlines()])
+
+    def is_valid_pos(self, pos: Pos) -> bool:
+        i, j = pos
+        return (0 <= i <= self.height) & (0 <= j <= self.width)
+
+
+@dataclass
+class Walk:
+    array: Array
+    curr: Pos = (0, 0)
+
+    def __next__(self) -> list[Self]:
+        i, j = self.curr
+        next_pos = filter(
+            self.array.is_valid_pos,
+            map(
+                lambda x: (x[0] + i, x[1] + j),
+                [
+                    (-1, 0),
+                    (0, -1), (0, 1),
+                    (1, 0)
+                ]
+            )
+        )
+
+        return [type(self)(self.array, pos) for pos in next_pos]
+
+
+terrain = Array.from_str(open('test.txt').read())
+print(terrain)
+w = Walk(terrain)
+print(w)
+print(next(w))
