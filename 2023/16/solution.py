ddf74b85a220d1a2049a5de0b5bd3f68ec36357d
thomas diasalves <26359654+diasalvt@users.noreply.github.com>
2024-01-16 16:33:46 +0100

    day 16

--- 2023/16/solution.py
+++ 2023/16/solution.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Self
 
 
@@ -6,8 +6,8 @@ Pos = tuple[int, int]
 Dir = str  # N, S, E, W
 
 DIRS: dict[Dir, tuple[int, int]] = {
-    'N': (1, 0),
-    'S': (-1, 0),
+    'N': (-1, 0),
+    'S': (1, 0),
     'E': (0, 1),
     'W': (0, -1)
 }
@@ -28,44 +28,115 @@ class Array:
         i, j = pos
         return (0 <= i <= self.height) & (0 <= j <= self.width)
 
-    def neighbours(self, pos: Pos) -> list[Pos]:
-        i, j = pos
-        return list(filter(
-            self.is_valid_pos,
-            map(
-                lambda x: (x[0] + i, x[1] + j),
-                [
-                    (-1, 0),
-                    (0, -1), (0, 1),
-                    (1, 0)
-                ]
-            )
-        ))
+    def __getitem__(self, key: tuple[int, int]) -> Any:
+        i, j = key
+        if (i < 0) or (j < 0):
+            raise IndexError
+        return self.arr[key[0]][key[1]]
 
 
-@dataclass
+@dataclass(frozen=True)
 class PlayerState:
-    pos: Pos = (0, 0)
-    dir: Dir
+    pos: Pos = (0, -1)
+    dir: Dir = 'E'
 
 
 @dataclass
 class GameState:
     array: Array
-    players: list[PlayerState]
-    past_players_states: set[PlayerState]
+    players: list[PlayerState] = field(default_factory=lambda: [PlayerState()])
+    past_players_states: set[PlayerState] = field(
+        default_factory=set
+    )
 
-    def move_player(self, player: PlayerState) -> list[PlayerState]:
+    def _move_player(self, player: PlayerState) -> list[PlayerState]:
+        """
+            Return True if GameState changed.
+        """
         i, j = player.pos
-        shift_i, shift_j = player.dir
-        next_pos = (i + shift_i, j + shift_j)
-        if self.array.is_valid(next_pos):
+        shift_i, shift_j = DIRS[player.dir]
+        next_pos = i + shift_i, j + shift_j
+        try:
+            next_val = self.array[next_pos]
 
-        return [type(self)(self.array, pos) for pos in next_pos]
+        except IndexError:
+            return []
+
+        match (next_val, player.dir):
+            case ('.', _):
+                return [PlayerState(next_pos, player.dir)]
+            case ('|', dir) if dir in ['N', 'S']:
+                return [PlayerState(next_pos, player.dir)]
+            case ('|', dir) if dir in ['E', 'W']:
+                return [
+                    PlayerState(next_pos, 'N'),
+                    PlayerState(next_pos, 'S')
+                ]
+            case ('-', dir) if dir in ['N', 'S']:
+                return [
+                    PlayerState(next_pos, 'W'),
+                    PlayerState(next_pos, 'E')
+                ]
+            case ('-', dir) if dir in ['W', 'E']:
+                return [PlayerState(next_pos, player.dir)]
+            case ('/', 'N'):
+                return [PlayerState(next_pos, 'E')]
+            case ('/', 'S'):
+                return [PlayerState(next_pos, 'W')]
+            case ('/', 'W'):
+                return [PlayerState(next_pos, 'S')]
+            case ('/', 'E'):
+                return [PlayerState(next_pos, 'N')]
+            case ('\\', 'N'):
+                return [PlayerState(next_pos, 'W')]
+            case ('\\', 'S'):
+                return [PlayerState(next_pos, 'E')]
+            case ('\\', 'W'):
+                return [PlayerState(next_pos, 'N')]
+            case ('\\', 'E'):
+                return [PlayerState(next_pos, 'S')]
+            case other:
+                raise ValueError(f'{other} is not a valid state')
+
+    def move_players(self) -> bool:
+        self.players = [
+            new_player
+            for player in self.players
+            for new_player in self._move_player(player)
+        ]
+        next_players = set(self.players) - self.past_players_states
+        self.players = list(next_players)
+        self.past_players_states |= set(self.players)
+        return len(next_players) > 0
 
 
-terrain = Array.from_str(open('test.txt').read())
-print(terrain)
-w = Walk(terrain)
-print(w)
-print(next(w))
+def score(game: GameState) -> int:
+    while game.move_players():
+        pass
+    return len({s.pos for s in game.past_players_states})
+
+
+terrain = Array.from_str(open('input.txt').read())
+print(score(GameState(terrain)))
+
+
+def score_2(terrain: Array) -> int:
+    max_south = max(
+        score(GameState(terrain, [PlayerState((-1, i), 'S')]))
+        for i in range(terrain.width)
+    )
+    max_north = max(
+        score(GameState(terrain, [PlayerState((terrain.height, i), 'N')]))
+        for i in range(terrain.width)
+    )
+    max_east = max(
+        score(GameState(terrain, [PlayerState((i, -1), 'E')]))
+        for i in range(terrain.height)
+    )
+    max_west = max(
+        score(GameState(terrain, [PlayerState((i, terrain.width), 'W')]))
+        for i in range(terrain.height)
+    )
+    return max(max_south, max_north, max_east, max_west)
+
+print(score_2(terrain))
