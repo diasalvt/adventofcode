7599394c97426a286a4ffd78fa45b01e4352cb86
thomas diasalves <26359654+diasalvt@users.noreply.github.com>
2024-03-12 12:15:10 +0100

    this was not fun

--- 2023/21/solution.py
+++ 2023/21/solution.py
@@ -129,7 +129,6 @@ def run2(puzzle_map: Map, steps: int = 1) -> set[Pos]:
 
 
 # print(run2(puzzle_map, 26501365))
-# print(run2(test_puzzle_map2, 5000))
 
 
 def area(n: int) -> int:
@@ -145,7 +144,7 @@ def covered(n: int) -> int:
     for i in range(2, n + 1):
         c_n, c_n_1 = 4 * i + c_n_1, c_n
 
-    return c_n
+    return c_n_1
 
 
 def grid(
@@ -175,23 +174,48 @@ def grid(
     return repeated_map, sub_grid
 
 
-big_test, _ = grid(puzzle_map, 7)
-display(big_test, run(big_test, 130 + 2 * 131 + 65))
+def solve_puzzle(puzzle_map: Map, total_steps) -> int:
 
-total_steps = 26501365
-
-
-def ultimate_pos(puzzle_map: Map, steps: int) -> tuple[int, int]:
+    repeat = 5
+    big_test, sub_grid = grid(puzzle_map, repeat)
     width, height = map_dim(puzzle_map)
-    half_width = width // 2
-    if steps <= half_width:
-        return (steps + half_width, 0)
+    fill_puzzle_map = run(big_test, (repeat * width - 1) // 2)
 
-    steps -= (half_width + 1)
-    return steps % width, steps // width
+    sub_grid_count = {
+        (x, y): sum(1 for p in fill_puzzle_map if p in positions)
+        for (x, y), positions in sub_grid.items()
+    }
+
+    corners = sum(
+        sub_grid_count[(x, y)] for x, y in [(0, 2), (2, 0), (2, 4), (4, 2)]
+    )
+
+    def count_tiles_filled(puzzle_map: Map, steps: int) -> tuple[int, int]:
+        width, height = map_dim(puzzle_map)
+        return int(((2 * steps + 1) / width - 1) // 2) + 1
+
+    total_full_size = count_tiles_filled(puzzle_map, total_steps)
+
+    total_inner = (
+        covered(total_full_size - 1) * sub_grid_count[(2, 1)] +
+        covered(total_full_size - 2) * sub_grid_count[(2, 2)]
+    )
+
+    outer_diag_size = total_full_size - 1
+    total_outer_diag = outer_diag_size * sum(
+        sub_grid_count[(x, y)]
+        for x, y in [(1, 0), (1, 4), (3, 0), (3, 4)]
+    )
+
+    total_inner_diag = (outer_diag_size - 1) * sum(
+        sub_grid_count[(x, y)]
+        for x, y in [(1, 1), (1, 3), (3, 1), (3, 3)]
+    )
+
+    return (
+        corners + total_inner + total_outer_diag + total_inner_diag
+    )
 
 
-print(ultimate_pos(puzzle_map, total_steps))
-print(f'pawn when even steps {len(run(puzzle_map, 100))}')
-print(f'pawn when odd steps {len(run(puzzle_map, 101))}')
-
+puzzle_steps = 26501365
+print(solve_puzzle(puzzle_map, puzzle_steps))
