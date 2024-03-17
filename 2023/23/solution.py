d9816b5e051c1aebfcfa473f8f39e0f7000282ab
diasalvt <ufg@>
2024-03-17 16:05:57 +0100

    day 23 part 2 unfinished, bfs

--- 2023/23/solution.py
+++ 2023/23/solution.py
@@ -1,3 +1,6 @@
+from functools import cache
+from collections import deque
+
 Pos = complex
 Dir = complex
 
@@ -18,7 +21,7 @@ def load(filename: str) -> Plan:
     with open(filename) as f:
         for i, row in enumerate(f):
             for j, c in enumerate(row):
-                pos = i + j * 1j
+                pos = j + i * 1j
                 if c == '.':
                     positions.add(pos)
                 if c in _directions:
@@ -32,7 +35,7 @@ def neighbours(plan: Plan, current_pos: Pos) -> set[Pos]:
     positions, forced_dir = plan
 
     possible_dirs = (
-        forced_dir[current_pos]
+        {forced_dir[current_pos]}
         if current_pos in forced_dir
         else {1, -1, 1j, -1j}
     )
@@ -40,14 +43,88 @@ def neighbours(plan: Plan, current_pos: Pos) -> set[Pos]:
     return {
         current_pos + dir
         for dir in possible_dirs
-        if (current_pos + dir) in plan
+        if (current_pos + dir) in positions
     }
 
 
-test_plan, test_forced_dir = load('test.txt')
+test_plan = load('test.txt')
 plan = load('input.txt')
-print(test_plan)
 
 
 def longest_path(plan: Plan) -> int:
-    if pos == 
+
+    start = next(p for p in plan[0] if p.imag == 0)
+    end = next(
+        p 
+        for p in plan[0]
+        if p.imag == max(p.imag for p in plan[0])
+    )
+
+    @cache
+    def rec_longest_path(current_pos: Pos, seen: frozenset[Pos]) -> int:
+        if current_pos == end:
+            return 1
+
+        next_pos = neighbours(plan, current_pos) - seen
+        steps = 1
+        while len(next_pos) == 1:
+            current_pos = next_pos.pop()
+            seen |= {current_pos}
+            if current_pos == end:
+                return steps
+            next_pos = neighbours(plan, current_pos) - seen
+            steps += 1
+
+        neighbours_longest_path = (
+            filter(
+                lambda x: x >= 0,
+                map(
+                    lambda x: rec_longest_path(x, seen | {x}),
+                    next_pos
+                )
+            )
+        )
+
+        return max(
+           (n + steps for n in neighbours_longest_path),
+           default=-1
+        )
+
+    return rec_longest_path(start, frozenset({start}))
+
+
+print(longest_path(plan))
+positions, forced_dirs = plan
+
+# Too slow
+# print(longest_path((positions, {})))
+
+
+def bfs(plan: Plan) -> int:
+    start = next(p for p in plan[0] if p.imag == 0)
+    end = next(
+        p 
+        for p in plan[0]
+        if p.imag == max(p.imag for p in plan[0])
+    )
+
+    states_to_explore = deque([(0, start, set())])
+    max_length = 0
+    i = 0
+    while states_to_explore :
+        length, pos, seen = states_to_explore.popleft()
+        print(i := i + 1)
+
+        # Test if we can beat the current max_length
+        # if (length + len(plan[0] - seen)) > max_length:
+        if pos == end:
+            if length > max_length:
+                max_length = length
+        else:
+            for n in neighbours(plan, pos) - seen:
+                states_to_explore.append((length + 1, n, seen | {n}))
+
+    return max_length
+
+
+print(bfs(test_plan))
