b97ffecb78436a6ac3fc03ec2290769487f8c11e
thomas diasalves <26359654+diasalvt@users.noreply.github.com>
2024-03-20 22:47:15 +0100

    part2, compress graph

--- 2023/23/solution.py
+++ 2023/23/solution.py
@@ -1,5 +1,5 @@
 from functools import cache
-from collections import deque
+from collections import deque, defaultdict
 
 Pos = complex
 Dir = complex
@@ -104,7 +104,28 @@ def is_node(plan: Plan, pos: Pos) -> bool:
     return len(neighbours(plan, pos)) > 2
 
 
-def bfs(plan: Plan) -> int:
+Graph = dict[Pos, dict[Pos, int]]
+
+
+def bfs_next_node(plan: Plan, start: Pos, special_nodes: set[Pos]) -> int:
+
+    states_to_explore = deque([(0, start, set())])
+    nodes_distance = defaultdict(int)
+
+    while states_to_explore:
+        length, pos, seen = states_to_explore.popleft()
+
+        if pos != start and pos in special_nodes:
+            if length > nodes_distance[pos]:
+                nodes_distance[pos] = length
+        else:
+            for n in neighbours(plan, pos) - seen:
+                states_to_explore.append((length + 1, n, seen | {pos}))
+
+    return nodes_distance
+
+
+def build_graph(plan: Plan) -> Graph:
     start = next(p for p in plan[0] if p.imag == 0)
     end = next(
         p
@@ -112,53 +133,47 @@ def bfs(plan: Plan) -> int:
         if p.imag == max(p.imag for p in plan[0])
     )
 
+    nodes = (
+        {start, end} |
+        {p for p in plan[0] if is_node(plan, p)}
+    )
+
+    return {
+        n: bfs_next_node(plan, n, nodes)
+        for n in nodes
+    }
+
+
+def longest_path_graph(g: Graph, start: Pos, end: Pos):
     states_to_explore = deque([(0, start, set())])
+
     max_length = 0
-    i = 0
     while states_to_explore:
         length, pos, seen = states_to_explore.popleft()
 
-        # Test if we can beat the current max_length
-        # if (length + len(plan[0] - seen)) > max_length:
         if pos == end:
             if length > max_length:
                 max_length = length
         else:
-            for n in neighbours(plan, pos) - seen:
-                states_to_explore.append((length + 1, n, seen | {n}))
+            for next_pos, dist in g[pos].items():
+                if next_pos not in seen:
+                    states_to_explore.append(
+                        (length + dist, next_pos, seen | {pos})
+                    )
 
     return max_length
 
 
-def bfs2(plan: Plan) -> int:
+def solve(plan: Plan) -> int:
     start = next(p for p in plan[0] if p.imag == 0)
     end = next(
         p
         for p in plan[0]
         if p.imag == max(p.imag for p in plan[0])
     )
-
-    states_to_explore = deque([(0, start, (-1, set()))])
-    end_length = []
-    while states_to_explore:
-        length, pos, (prev_pos, seen_nodes) = states_to_explore.popleft()
-
-        if pos == end:
-            end_length.append(length)
-        else:
-            seen = {prev_pos} | seen_nodes
-            for n in neighbours(plan, pos) - seen:
-                if is_node(plan, n):
-                    next_seen_nodes = seen_nodes | {n}               
-                else:
-                    next_seen_nodes = seen_nodes
-                states_to_explore.append(
-                    (length + 1, n, (pos, next_seen_nodes))
-                )
-
-    return max(end_length)
+    graph = build_graph(plan)
+    return longest_path_graph(graph, start, end)
 
 
-# print(bfs(test_plan))
-positions, _ = test_plan
-print(bfs2((positions, {})))
+positions, _ = plan
+print(solve((positions, {})))
