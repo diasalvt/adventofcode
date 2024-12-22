7fb96a6aeb171d635fde3705eff37badf375370d
thomas diasalves <26359654+diasalvt@users.noreply.github.com>
2024-12-22 18:19:58 +0100

    day 3

--- 2024/3/solution.py
+++ 2024/3/solution.py
@@ -10,4 +10,27 @@ def parse(filename: str) -> list[tuple[int, int]]:
 
 
 puzzle = parse('input.txt')
-print(puzzle)
+print(sum(a * b for a, b in puzzle))
+
+
+def parse(filename: str) -> list[tuple[int, int]]:
+    with open(filename) as f:
+        return re.findall(
+            r"mul\((\d{1,3}),(\d{1,3})\)|(do|don't)\(\)",
+            f.read()
+        )
+
+
+do = True
+res = 0
+for m1, m2, do_or_dont in parse('input.txt'):
+    match do_or_dont:
+        case "do":
+            do = True
+        case "don't":
+            do = False
+        case _:
+            if do:
+                res += int(m1) * int(m2)
+
+print(res)
