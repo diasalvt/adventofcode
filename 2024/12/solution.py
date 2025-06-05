2fc892c8ed1bc1e1e6fed69564c91538d56e39ad
thomas diasalves <26359654+diasalvt@users.noreply.github.com>
2025-06-05 16:47:09 +0200

    part 2

--- 2024/12/solution.py
+++ 2024/12/solution.py
@@ -1,7 +1,9 @@
 from collections import Counter, defaultdict
-from itertools import chain
+from itertools import tee, islice
+from typing import Iterator, Iterable
 
-type Garden = dict[complex, str]
+Plant = str | int
+Garden = dict[complex, Plant]
 
 
 def load(filename: str) -> Garden:
@@ -23,40 +25,40 @@ def neighbours(
 
     return tuple(
         pos + i + j * 1j
-        for i, j in {(-1, 0), (0, -1), (1, 0), (0, 1)}
+        for i, j in {(-1, 0), (0, -1), (0, 1), (1, 0)}
         if (pos + (i + j * 1j)) in garden
     )
 
-def contiguous(garden: Garden, pos: complex) -> list[complex]:
-    plant = garden[pos]
-    seen = set()
-
-    def _contiguous_plant(
-        garden: Garden, pos: complex, seen: set[complex] = seen
-    ) -> list[complex]:
-        seen |= {pos}
-        return [pos] + list(chain.from_iterable(
-            _contiguous_plant(garden, n)
-            for n in neighbours(garden, pos)
-            if (garden[n] == plant) and (n not in seen)
-        ))
-
-    return _contiguous_plant(garden, pos, seen=seen)
-
-
 # def contiguous(garden: Garden, pos: complex) -> list[complex]:
 #     plant = garden[pos]
-#     to_explore = [pos]
 #     seen = set()
 
-#     while to_explore:
-#         current = to_explore.pop()
-#         if current not in seen:
-#             for n in neighbours(garden, current):
-#                 if garden[n] == plant:
-#                     to_explore.append(n)
-#             seen |= {current}
-#     return list(seen)
+#     def _contiguous_plant(
+#         garden: Garden, pos: complex, seen: set[complex] = seen
+#     ) -> list[complex]:
+#         seen |= {pos}
+#         return [pos] + list(chain.from_iterable(
+#             _contiguous_plant(garden, n)
+#             for n in neighbours(garden, pos)
+#             if (garden[n] == plant) and (n not in seen)
+#         ))
+
+#     return _contiguous_plant(garden, pos, seen=seen)
+
+
+def contiguous(garden: Garden, pos: complex) -> list[complex]:
+    plant = garden[pos]
+    to_explore = [pos]
+    seen = set()
+
+    while to_explore:
+        current = to_explore.pop()
+        if current not in seen:
+            for n in neighbours(garden, current):
+                if garden[n] == plant:
+                    to_explore.append(n)
+            seen |= {current}
+    return list(seen)
 
 
 def replace_letter_by_number(garden: Garden) -> Garden:
@@ -68,7 +70,7 @@ def replace_letter_by_number(garden: Garden) -> Garden:
                 new_garden[c] = i
             i += 1
     return new_garden
-    
+
 
 def areas(garden: Garden) -> Counter:
     return Counter(garden.values())
@@ -83,7 +85,7 @@ def perimeter(garden: Garden) -> defaultdict[str, int]:
         )
         d[plant] += border
     return d
-    
+
 
 def part1(garden: Garden) -> int:
     new_garden = replace_letter_by_number(garden)
@@ -95,4 +97,59 @@ def part1(garden: Garden) -> int:
         for plant, count in garden_areas.items()
     )
 
+
 print(part1(garden))
+
+
+def nwise(it: Iterable, n: int) -> Iterator:
+    return zip(*(islice(it, s, None) for s, it in enumerate(tee(it, n))))
+
+
+def count_corners_pos(garden: Garden, pos: complex) -> int:
+    def pos_around(
+        garden: Garden, pos: complex
+    ) -> tuple[complex]:
+
+        return list(
+            nwise((
+                pos + i + j * 1j
+                for i, j in [
+                    (-1, 0), (-1, -1), (0, -1), (1, -1),
+                    (1, 0), (1, 1), (0, 1),
+                    (-1, 1), (-1, 0)
+                ]
+            ), 3)
+        )
+
+    outer_corners = sum(
+        garden[pos] not in {garden.get(n_1, -1), garden.get(n_3, -1)}
+        for n_1, n_2, n_3 in pos_around(garden, pos)
+        if abs(n_2 - pos) > 1
+    )
+    interior_corners = sum(
+        (
+            (garden.get(n_1, -1) == garden.get(n_3, -1) == garden[pos]) and
+            (garden.get(n_2, -1) != garden[pos])
+        )
+        for n_1, n_2, n_3 in pos_around(garden, pos)
+        if abs(n_2 - pos) > 1
+    )
+
+    return outer_corners + interior_corners
+
+
+def part2(garden: Garden) -> int:
+    new_garden = replace_letter_by_number(garden)
+    garden_areas = areas(new_garden)
+
+    garden_modified_perimeter = defaultdict(int)
+    for pos, plant in new_garden.items():
+        garden_modified_perimeter[plant] += count_corners_pos(new_garden, pos)
+
+    return sum(
+        count * garden_modified_perimeter[plant]
+        for plant, count in garden_areas.items()
+    )
+
+
+print(part2(garden))
