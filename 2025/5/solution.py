6c56af3b11c4dce6906a8b606e25407642435927
diasalvt <ufg@>
2025-12-07 19:15:52 +0100

    day 5, 2025

--- 2025/5/solution.py
+++ 2025/5/solution.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def load(filename: str) -> tuple[list[range], list[int]]:
     with open(filename) as f:
         ranges_str, data_str = f.read().split('\n\n')
@@ -25,7 +28,46 @@ print(
 )
 
 
-def union(r1: range, r2: range) -> list[range]:
-    
-def add_range(r: range, l_sorted_ranges: list[range]) -> list[range]:
-    start, end = r.start, r.stop - 1
+def before_after(r1: range, r2: range) -> tuple[list[range], range | None]:
+    start_checks = (
+        r1.start < r2.start,
+        r2.start <= r1.start < r2.stop,
+        r1.start >= r2.stop
+    )
+    end_checks = (
+        r1.stop <= r2.start,
+        r2.start < r1.stop <= r2.stop,
+        r1.stop > r2.stop
+    )
+    match start_checks, end_checks:
+        case _, (True, _, _):
+            return ([r1, r2], None)
+        case (_, _, True), _:
+            return ([r2], r1)
+        case (_, True, _), (_, True, _):
+            return ([r2], None)
+        case (_, True, _), (_, _, True):
+            return ([range(r2.start, r2.stop)], range(r2.stop, r1.stop))
+        case (True, _, _), (_, True, _):
+            return ([range(r1.start, r2.stop)], None)
+        case (True, _, _), (_, _, True):
+            return ([range(r1.start, r2.stop)], range(r2.stop, r1.stop))
+        case _:
+            raise ValueError(f'Unexpected ranges {r1=}, {r2=}')
+
+
+def add_range(l_sorted_ranges: list[range], r: range) -> list[range]:
+    if len(l_sorted_ranges) == 0:
+        return [r]
+
+    first_range, *rest_ranges = l_sorted_ranges
+
+    ranges, rest = before_after(r, first_range)
+    if rest is None:
+        return ranges + rest_ranges
+
+    return ranges + add_range(rest_ranges, rest)
+
+
+result = sum(map(len, reduce(add_range, ranges, [])))
+print(result)
