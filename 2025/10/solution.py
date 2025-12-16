89f56a97c6c7a3c1644f572442ca15db116533e7
diasalvt <ufg@>
2025-12-16 14:13:27 +0100

    day 10 part 1 2025

---
+++ 2025/10/solution.py
@@ -0,0 +1,56 @@
+from itertools import product, compress
+from functools import reduce
+import operator
+
+
+Puzzle = tuple[
+    tuple[bool, ...], list[int],
+    set[int]
+]
+
+
+def load(filename: str) -> list[Puzzle]:
+    with open(filename) as f:
+        lines = f.read().splitlines()
+
+    result = []
+    for line in lines:
+        press, *buttons, joltage = line.split()
+        press = tuple(
+            True if c == '#' else False for c in press[1:-1]
+        )
+        button_positions = [
+            tuple(int(b_i) for b_i in button[1:-1].split(','))
+            for button in buttons
+        ]
+        buttons = [
+            tuple(i in button_position for i in range(len(press)))
+            for button_position in button_positions
+        ]
+
+        joltage = set(
+            int(j_i)
+            for j_i in joltage[1:-1].split(',')
+        )
+        result.append((press, buttons, joltage))
+
+    return result
+
+
+def xor(*t: tuple[bool, ...]) -> tuple[bool, ...]:
+    return tuple(reduce(operator.xor, t_i) for t_i in zip(*t))
+
+
+puzzles = load('test.txt')
+
+
+result = sum(
+    min(
+        sum(selection)
+        for selection in product((0, 1), repeat=len(buttons))
+        if xor(*compress(buttons, selection)) == press
+    )
+    for press, buttons, _ in puzzles
+)
+
+print(result)
